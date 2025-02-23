0a6681aaf0c90665bea60b7c34131704aeebcb10
Sameet Keshav <sameet.keshav@gmail.com>
2025-02-23 19:05:41 +0200

    wip: fnm and node install

--- config/home-manager/zsh/fnm.nix
+++ config/home-manager/zsh/fnm.nix
@@ -1,28 +1,50 @@
 { config, pkgs, lib, ... }:
+let
+  fnmInstallScript = ''
+    mkdir -p $HOME/.fnm
+    export FNM_DIR=$HOME/.fnm
+    eval "$(fnm env)"
+    for version in ${lib.concatStringsSep " " config.programs.fnm.versions}; do
+      fnm install --arch x64 $version
+    done
+    fnm default ${config.programs.fnm.defaultVersion}
+  '';
+in
 {
-  programs.fnm = {
-    enable = true;
-    versions = [ "18.20.6" "20.18.3" ]; # Specify the versions you want to install
-    defaultVersion = "18.20.6"; # Set the default version
+  options.programs.fnm = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable fnm (Fast Node Manager)";
+    };
+    versions = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = ["18.20.6" "20.18.3"];
+      description = "List of Node.js versions to install with fnm";
+    };
+    defaultVersion = lib.mkOption {
+      type = lib.types.str;
+      default = "18.20.6";
+      description = "Default Node.js version to set with fnm";
+    };
   };
 
-  home.packages = [
-    pkgs.fnm
-  ];
+  config = lib.mkIf config.programs.fnm.enable {
+    home.packages = [ pkgs.fnm ];
 
-  home.sessionVariables = {
-    FNM_DIR = "$HOME/.fnm";
-    PATH = "${config.home.homeDirectory}/.fnm:${config.home.homeDirectory}/.fnm/aliases/default/bin:$PATH";
-  };
+    home.sessionVariables = {
+      FNM_DIR = "$HOME/.fnm";
+      PATH = "${config.home.homeDirectory}/.fnm:${config.home.homeDirectory}/.fnm/aliases/default/bin:$PATH";
+    };
 
-  home.activation = {
-    fnm = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      mkdir -p $HOME/.fnm
+    home.activation = {
+      fnm = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        ${fnmInstallScript}
+      '';
+    };
+
+    programs.zsh.initExtra = ''
       eval "$(fnm env)"
-      for version in ${lib.concatStringsSep " " config.programs.fnm.versions}; do
-        fnm install --arch x64 $version
-      done
-      fnm default ${config.programs.fnm.defaultVersion}
     '';
   };
 }
