edbdece927791a3cb72f90dba4dd520ed4a780ba
Sameet Keshav <sameet.keshav@gmail.com>
2025-04-23 12:58:33 +0200

    feat: Add flutter to PATH

--- config/home-manager/zsh/default.nix
+++ config/home-manager/zsh/default.nix
@@ -121,7 +121,7 @@
       EDITOR = "nvim";
 
       # Oracle Instant Client
-      PATH="/usr/local/lib/instantclient_19_8:$PATH";
+      PATH="/usr/local/lib/instantclient_19_8:$HOME/development/flutter/bin:$PATH";
       ORACLE_HOME=/usr/local/lib/instantclient_19_8;
       DYLD_LIBRARY_PATH=/usr/local/lib/instantclient_19_8;
       OCI_LIB_DIR=/usr/local/lib/instantclient_19_8;
